806a8a112d13e729ab77e112de2de6507b53f86e
fallingsprings <tim@Tims-MacBook.local>
2015-10-31 16:19:07 -0400

    ability to turn off validation

--- html5-input-types/html5-input-types.php
+++ html5-input-types/html5-input-types.php
@@ -12,12 +12,14 @@
                                'min'=>'',
                                'max'=>'',
                                'step'=>'',
+                               'validate'=>'1',
                               ),
                         $params);
             $attr['value'] = trim($attr['value']);
             $attr['min'] = trim($attr['min']);
             $attr['max'] = trim($attr['max']);
             $attr['step'] = trim($attr['step']);
+            $attr['validate'] = trim($attr['validate']);
 
             return $attr;
         }
@@ -78,7 +80,9 @@
     class HTML5InputEmail extends HTML5InputTypes{
         function validate(){
             global $FUNCS;
-            $this->validator = 'email';           
+            if ( $this->validate != '0' ){
+                $this->validator = 'email'; 
+            }
             return parent::validate();
         }
     }
@@ -86,7 +90,9 @@
     class HTML5InputUrl extends HTML5InputTypes{
         function validate(){
             global $FUNCS;
-            $this->validator = 'url';           
+            if ( $this->validate != '0' ){
+                $this->validator = 'url';
+            }
             return parent::validate();
         }
     }
@@ -104,7 +110,7 @@
                         $params) );
             
             //sanitize color value.
-            $value = trim($value);
+            $value = strtolower( trim($value) );
             $pattern = '/^#([A-Fa-f0-9]{6})$/';
             
             if ( !preg_match( $pattern, $value ) ){
@@ -113,17 +119,19 @@
             
             $attr['value'] = $value;
             return $attr;  
-
          }
         
         function validate(){
             global $FUNCS;
+            if ( $this->validate != '0' ){            
                 $value = $this->get_data();
                 $pattern = '/^#([A-Fa-f0-9]{6})$/';
+                
                 if ( !preg_match( $pattern, $value ) ){
                     $this->err_msg = 'Not a valid hexadecimal color value.';
                     return false;
                 }
+            }
             return parent::validate();
         }
     }
@@ -173,61 +181,63 @@
                 return parent::validate(); // will handle 'required'
             }
 
-            $value = $this->get_data();
+            if ( $this->validate != '0' ){
+                $value = $this->get_data();
 
 // $value=100; // fudge values for testing invalid inputs
 
-            //Is it a number?
-            if( !is_numeric($value) ){
-                $this->err_msg = 'Not a number';
-                return false;
-            }
-
-            //Is it in range?
-            if( $this->min != '' && $value < $this->min ){
-                $this->err_msg = 'Out of range. Minimum value is '.$this->min.'.' ;
-                return false;
-            }
-            if( $this->max != '' && $value > $this->max ){
-                $this->err_msg = 'Out of range. Maximum value is '.$this->max.'.' ;
-                return false;
-            }
-
-            //Is it a valid number?
-            $step = $this->step;
-
-            //Must be a multiple of step.
-            $min = strlen( $this->min ) ? $this->min : 0;
-            $val = ( $value < 0 ) ? ( $value + $min ) : ( $value - $min );
-            $val = round( ($val / $step), 2 );
-
-            if( $val != intval($val) ){ // not an exact multiple of step
-                $val = intval($val) * $step;    // discard the fractional part
-                if( $value < 0 ){
-                    $higher = $val - $min;
-                    $lower = $higher - $step;
-                }
-                else{
-                    $lower = $val + $min;
-                    $higher = $lower + $step;
+                //Is it a number?
+                if( !is_numeric($value) ){
+                    $this->err_msg = 'Not a number';
+                    return false;
                 }
 
-                if( $this->max != '' && $higher > $this->max ){
-                    $higher = null;
+                //Is it in range?
+                if( $this->min != '' && $value < $this->min ){
+                    $this->err_msg = 'Out of range. Minimum value is '.$this->min.'.' ;
+                    return false;
                 }
-                if( $this->min != '' && $lower < $this->min ){
-                    $lower = null;
-                }
-
-                if( !is_null($lower) && !is_null($higher) ){
-                    $this->err_msg = 'Not a valid number. The two nearest valid numbers are  '.$lower.' and '.$higher.'.';
-                }
-                else{
-                    $valid_value = ( !is_null($lower) ) ? $lower : $higher;
-                    $this->err_msg = 'Not a valid number. The nearest valid number is '.$valid_value.'.';
+                if( $this->max != '' && $value > $this->max ){
+                    $this->err_msg = 'Out of range. Maximum value is '.$this->max.'.' ;
+                    return false;
                 }
 
-                return false;
+                //Is it a valid number?
+                $step = $this->step;
+
+                //Must be a multiple of step.
+                $min = strlen( $this->min ) ? $this->min : 0;
+                $val = ( $value < 0 ) ? ( $value + $min ) : ( $value - $min );
+                $val = round( ($val / $step), 2 );
+
+                if( $val != intval($val) ){ // not an exact multiple of step
+                    $val = intval($val) * $step;    // discard the fractional part
+                    if( $value < 0 ){
+                        $higher = $val - $min;
+                        $lower = $higher - $step;
+                    }
+                    else{
+                        $lower = $val + $min;
+                        $higher = $lower + $step;
+                    }
+
+                    if( $this->max != '' && $higher > $this->max ){
+                        $higher = null;
+                    }
+                    if( $this->min != '' && $lower < $this->min ){
+                        $lower = null;
+                    }
+
+                    if( !is_null($lower) && !is_null($higher) ){
+                        $this->err_msg = 'Not a valid number. The two nearest valid numbers are  '.$lower.' and '.$higher.'.';
+                    }
+                    else{
+                        $valid_value = ( !is_null($lower) ) ? $lower : $higher;
+                        $this->err_msg = 'Not a valid number. The nearest valid number is '.$valid_value.'.';
+                    }
+
+                    return false;
+                }
             }
 
             // Values are fine. Let parent handle custom validators, if any specified
@@ -253,6 +263,182 @@
         }
     }
 
+       class HTML5InputDate extends HTML5InputTypes{
+        function handle_params( $params ){
+            global $FUNCS;
+
+            $attr = parent::handle_params( $params );
+            $pattern = '/^(\d{4})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/';
+
+
+            // sanitize parameters
+            if( $attr['value'] != '' && !preg_match($pattern, $attr['value']) ){
+                die( "ERROR: Tag \"input\" type \"date\" - 'value' attribute must be a valid date string (YYYY-MM-DD)." );
+            }
+            if ( $attr['min'] != '' && !preg_match($pattern, $attr['min']) ){
+                die("ERROR: Tag \"input\" type \"date\" - 'min' attribute not a valid date string (YYYY-MM-DD).");
+            }
+            if( $attr['max'] != '' && !preg_match($pattern, $attr['max']) ){
+                die( "ERROR: Tag \"input\" type \"date\" - 'max' attribute not a valid date string (YYYY-MM-DD)." );
+            }
+            if ( $attr['min'] != '' && $attr['max'] != '' && $attr['min'] >= $attr['max'] ){
+                die( "ERROR: Tag \"input\" type \"date\" - 'max' attribute must be greater than 'min' attribute." );
+            }
+            if( $attr['step'] == '' ){
+                $attr['step'] = '1';
+            }
+            else{
+                if( !is_numeric($attr['step']) || $attr['step'] <= 0 ){
+                    die( "ERROR: Tag \"input\" type \"date\" - 'step' attribute must be a positive number." );
+                }
+            }
+            return $attr;
+        }
+
+        function validate(){
+            global $FUNCS;
+
+            if( $this->is_empty() ){
+                return parent::validate(); // will handle 'required'
+            }
+
+            if ( $this->validate != '0' ){
+                $value = $this->get_data();
+                $pattern = '/^(\d{4})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/';
+            
+                if ( !preg_match( $pattern, $value ) ){
+                    $this->err_msg = 'Not a valid date.';
+                    return false;
+                }
+                        
+                $date = explode('-', $value);
+                $month = $date[1];
+                $day = $date[2];
+                
+                //Shorter months
+                if ( $day == '31' && ($month == '02' || $month == '04' || $month == '06' || $month == '09' || $month == '11') ){
+                    $this->err_msg = 'Not a valid date.';
+                    return false;
+                }
+                //February
+                if ( $month == '02' && $day == '30' ){
+                    $this->err_msg = 'Not a valid date.';
+                    return false;
+                }
+                //Leap Year
+                if ( $month == '02' && !date('L', $value) && $day == '29' ){
+                    $this->err_msg = 'Not a valid date.';
+                    return false;
+                }
+                    
+                //Is it in range?
+                if ( $this->min !='' && $value < $this->min ){
+                    $this->err_msg = 'Out of range. Minimum value is '.$this->min.'.' ;
+                    return false;
+                }
+                if ( $this->max && $value > $this->max ){
+                    $this->err_msg = 'Out of range. Maximum value is '.$this->max.'.' ;
+                    return false;
+                }
+            }
+
+        // Values are fine. Let parent handle custom validators, if any specified
+        return parent::validate();
+        }
+    }
+
+    class HTML5InputTime extends HTML5InputTypes{
+        function handle_params( $params ){
+            global $FUNCS;
+
+            $attr = parent::handle_params( $params );
+            $pattern = '/^(([0-1][0-9])|([2][0-3])):([0-5][0-9])(:([0-5][0-9])(.([0-9]?[0-9]?[0-9]))?)?$/';
+
+
+            // sanitize parameters
+            if( $attr['value'] != '' && !preg_match($pattern, $attr['value']) ){
+                die( "ERROR: Tag \"input\" type \"time\" - 'value' attribute must be a valid time string (00:00{.00.000})." );
+            }
+            if ( $attr['min'] != '' && !preg_match($pattern, $attr['min']) ){
+                die("ERROR: Tag \"input\" type \"time\" - 'min' attribute not a valid time string (00:00{.00.000}).");
+            }
+            if( $attr['max'] != '' && !preg_match($pattern, $attr['max']) ){
+                die( "ERROR: Tag \"input\" type \"time\" - 'max' attribute not a valid time string (00:00{.00.000})." );
+            }
+            if ( $attr['min'] != '' && $attr['max'] != '' ) {
+                // Normalize time values before comparing.
+                $min = explode(':', $attr['min']);
+                $hours = $min[0];
+                $minutes = $min[1];
+                $seconds = $min[2];
+                if ( !$seconds ) $seconds = '00';
+                $min = $hours.$minutes.$seconds;
+
+                $max = explode(':', $attr['max']);
+                $hours = $max[0];
+                $minutes = $max[1];
+                $seconds = $max[2];
+                if ( !$seconds ) $seconds = '00';
+                $max = $hours.$minutes.$seconds;
+
+                if( $min > $max ){
+                die( "ERROR: Tag \"input\" type \"time\" - 'max' attribute must be greater than 'min' attribute." );
+                }
+            }
+            if( $attr['step'] == '' ){
+                $attr['step'] = '1';
+            }
+            else{
+                if( !is_numeric($attr['step']) || $attr['step'] <= 0 ){
+                    die( "ERROR: Tag \"input\" type \"time\" - 'step' attribute must be a positive number." );
+                }
+            }
+            return $attr;
+        }
+
+        function validate(){
+            global $FUNCS;
+
+            if( $this->is_empty() ){
+                return parent::validate(); // will handle 'required'
+            }
+
+            if ( $this->validate != '0' ){
+                $value = $this->get_data();
+                $pattern = '/^(([0-1][0-9])|([2][0-3])):([0-5][0-9])(:([0-5][0-9])(.([0-9]?[0-9]?[0-9]))?)?$/';
+            
+                if ( !preg_match( $pattern, $value ) ){
+                    $this->err_msg = 'Not a valid time.';
+                    return false;
+                }
+                                        
+                //Is it in range?
+                    // First normalize values, then compare.
+                    function normalize_time($value){
+                        $time = explode(':', $value);
+                        $hours = $time[0];
+                        $minutes = $time[1];
+                        $seconds = $time[2];
+                        if ( !$seconds ) $seconds = '00';
+                        $time = $hours.$minutes.$seconds;
+                    
+                        return $time;
+                    }
+                if ( $this->min !='' && normalize_time($value) < normalize_time($this->min) ){
+                    $this->err_msg = 'Out of range. Minimum value is '.$this->min.'.' ;
+                    return false;
+                }
+                if ( $this->max !='' && normalize_time($value) > normalize_time($this->max) ){
+                    $this->err_msg = 'Out of range. Maximum value is '.$this->max.'.' ;
+                    return false;
+                }
+            }
+
+        // Values are fine. Let parent handle custom validators, if any specified
+        return parent::validate();
+        }
+    }
+
     $FUNCS->register_udform_field( 'search', 'HTML5InputTypes' );
     $FUNCS->register_udform_field( 'tel', 'HTML5InputTypes' );
     $FUNCS->register_udform_field( 'email', 'HTML5InputEmail' );
@@ -260,3 +446,5 @@
     $FUNCS->register_udform_field( 'color', 'HTML5InputColor' );
     $FUNCS->register_udform_field( 'number', 'HTML5InputNumber' );
     $FUNCS->register_udform_field( 'range', 'HTML5InputRange' );
+    $FUNCS->register_udform_field( 'date', 'HTML5InputDate' );
+    $FUNCS->register_udform_field( 'time', 'HTML5InputTime' );
