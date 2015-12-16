122beddefe7c6ef294a18a24eca7f47947e1ce28
fallingsprings <tim@Tims-MacBook.local>
2015-12-15 22:45:55 -0500

    PayPal Donate Tag

---
+++ paypal_donate/README.md
@@ -0,0 +1,59 @@
+#PayPal Donate Button
+
+The paypal_donate tag is a lot like the paypal_button tag (http://docs.couchcms.com/tags-reference/paypal_button.html), but a little simpler. Unlike the paypal_button tag, it does not require a clonable template or external variables. All necessary information is passed through parameters of the tag. It can be used together with the paypal_processor tag (http://docs.couchcms.com/tags-reference/paypal_processor.html). See Core Concepts - PayPal (http://docs.couchcms.com/concepts/paypal.html) for a detailed discussion.
+
+##Parameters
+- image
+- processor
+- purpose
+- reference
+- amount
+- custom
+
+###image
+This parameter is used to set the image used as the button.
+You can either choose to use an image of your own or you may use one of the buttons made available by PayPal. Alternatively, you can use an ordinary submit button and style it with CSS.
+
+**Using a CSS button -**
+
+Provide the image parameter with the string "css:My Custom Text". This will create a submit button with the value "My Custom Text" and the class name "pp_submit" which can be used to apply your own styles to the button.
+
+	<cms:paypal_button image="css:Donate" />
+
+**Using your own image -**
+
+	<cms:paypal_button image="<cms:show k_site_link />/images/my_button.gif" />
+
+**Using PayPal provided images -**
+
+To use these images, set the image parameter to a number ranging from 0 to 6.
+
+	<cms:paypal_button image='3' />
+
+If not specified, the first image is the default. The numbers represent a range of PayPal donate button images.
+
+###processor
+
+The generated button provides PayPal with a link to the page that will process the IPN sent by it. By default this will be the link of the page the button is located on. If you have placed the paypal_processor tag, that handles the IPN, on some other page, set that page's link as this parameter.
+
+###purpose
+
+Set this to the name of the organization, fund, or campaign. If not set, the donor can fill in a text field on the PayPal site.
+
+###reference
+
+An optional field for specifying donation details.
+<cms:paypal_donate purpose='My Great Cause' reference='Spring Gala' />
+
+###amount
+
+You can specify an amount or leave it blank to allow donors to enter an amount on the PayPal website.
+
+###custom
+
+This is a pass-through variable for your own purposes. Donors won't see it. 
+
+##Variables
+
+This tag is self-closing and does not set any variables of its own.
+
---
+++ paypal_donate/paypal_donate.php
@@ -0,0 +1,89 @@
+<?php
+    
+    if ( !defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
+    
+    class PayPalDonate{
+        
+        function paypal_donate( $params, $node ){
+            global $CTX, $FUNCS, $PAGE;
+            if( count($node->children) ) {die("ERROR: Tag \"".$node->name."\" is a self closing tag");}
+
+            extract( $FUNCS->get_named_vars(
+                        array(
+                              'image'=>'',
+                              'processor'=>'',
+                              'return_url'=>'',
+                              'cancel_url'=>'',
+                              'custom'=>'',
+                              'purpose'=>'',
+                              'reference'=>'',
+                              'amount'=>''
+                             ),
+                        $params)
+                   );
+            $image = trim( $image );
+            $processor = trim( $processor );
+            $show_shipping = ( $show_shipping==1 ) ? 1 : 0;
+            $return_url = trim( $return_url );
+            $cancel_url = trim( $cancel_url );
+
+            $item_name = trim( $purpose );
+            $item_number = trim( $reference );
+            $amount = trim( $amount );
+
+            $return_url = ( $return_url ) ? $return_url : K_SITE_URL . $PAGE->link;
+            $cancel_url = ( $cancel_url ) ? $cancel_url : $return_url;
+            $processor = ( $processor ) ? $processor : $return_url;
+            $sep = ( strpos($processor, '?')===false ) ? '?' : '&';
+            $notify_url = $processor . $sep . 'paypal_ipn=1';
+
+                if( K_PAYPAL_USE_SANDBOX ){
+                    $paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+                }
+                else{
+                    $paypal_url = 'https://www.paypal.com/cgi-bin/webscr';
+                }
+
+                if( !$image ) $image=0;
+            if( $FUNCS->is_natural($image) ){
+                $arr_btns = array( 'btn_donate_SM.gif', 'btn_donate_LG.gif', 'btn_donateCC_LG.gif',
+                                  'x-click-but04.gif', 'x-click-butcc-donate.gif',
+                                  'x-click-but11.gif', 'x-click-but21.gif' );
+                $image_src = 'https://www.paypal.com/en_US/i/btn/'.$arr_btns[$image];
+            }
+            else{
+                $css = explode(':', $image);
+                if ( !$css[1] ){
+                    $image_src = $image;
+                }
+            }
+
+            $html .= '<form action="'.$paypal_url.'" method="post">';
+            $html .= '<input type="hidden" name="cmd" value="_donations"/>';
+            $html .= '<input type="hidden" name="business" value="'.K_PAYPAL_EMAIL.'"/>';
+            $html .= '<input type="hidden" name="item_name" value="'.$item_name.'"/>';
+            $html .= '<input type="hidden" name="item_number" value="'.$item_number.'"/>';
+            $html .= '<input type="hidden" name="amount" value="'.$amount.'"/>';
+            $html .= '<input type="hidden" name="no_note" value="1"/>';
+            $html .= '<input type="hidden" name="currency_code" value="'.K_PAYPAL_CURRENCY.'"/>';
+            $html .= '<input type="hidden" name="rm" value="2"/>';
+            $html .= '<input type="hidden" name="custom" value="'.$custom.'">';
+            $html .= '<input type="hidden" name="return" value="'.$return_url.'"/>';
+            $html .= '<input type="hidden" name="cancel_return" value="'.$cancel_url.'"/>';
+            $html .= '<input type="hidden" value="'.$notify_url.'" name="notify_url"/>';
+            if( $css[1] ){
+                $html .= '<input type="submit" class="pp_submit" value="'.trim( $css[1] ).'"/>';
+            }
+            else{
+                $html .= '<input type="image" border="0" alt="Make payments with PayPal - it\'s fast, free and secure!"';
+                $html .= ' name="submit" src="'.$image_src.'"/>';
+            }
+            $html .= '<img width="1" height="1" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" alt=""/>';
+            $html .= '</form>';
+
+
+            return $html;
+        }
+    }
+    
+    $FUNCS->register_tag( 'paypal_donate', array('PayPalDonate', 'paypal_donate') );
