4b106a6aae6c3411b44cf7b3246c31086f17816d
Tim Brelig <tim@fallingspringsflowers.com>
2016-07-11 22:59:08 -0400

    Redesigned the entire approach. Much better now.

--- character-counter/character-counter.php
+++ character-counter/character-counter.php
@@ -3,31 +3,19 @@
     if ( !defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
     
     class CharacterCounter{
-        
-    function character_counter( $params, $node ){ // for use within cms:config_list_view/config_form_view
-        global $CTX, $FUNCS, $AUTH;
-        if( $AUTH->user->access_level < K_ACCESS_LEVEL_SUPER_ADMIN ){ return; }
+        function character_counter( $params, $node ){
 
-        // get the 'config' object supplied by 'cms:config_form_view' tag
-        $arr_config = &$CTX->get_object( '__config', 'config_form_view' );
-        if( !is_array($arr_config) ){ return; }
-
-        if( count($node->children) ){
-            $code = $node->children;
-            $params = trim($code[0]->text);
+            if( count($node->children) ){
+                $code = $node->children;
+                $params = trim($code[0]->text);
             
-            $script = 'var my_counters=['.$params.'];';
-            $script .= file_get_contents(K_ADMIN_URL . 'addons/character-counter/js/characterCounter.min.js');
-        }else { return; }
+                $script = 'var my_counters=['.$params.'];';
+                $script .= file_get_contents(K_ADMIN_URL . 'addons/character-counter/js/characterCounter.min.js');
+            }else { return; }
         
-        if($arr_config['js']){ //cms:script object already exists
-            $arr_config['js'][0]->text .= $script;
-        }else { //no pre-existing script object
-            $code[0]->text = $script;        
-            $arr_config['js'] = $code;
-        }      
-    }        
-}
+        return $script;      
+        }        
+    }
 $FUNCS->register_tag( 'character_counter', array('CharacterCounter', 'character_counter') );
 //TODO: repeatable 'remove row' breaks 'add row' listener
 //Maybe wait for expected update to repeatable regions, though
--- character-counter/js/characterCounter.js
+++ character-counter/js/characterCounter.js
@@ -1,18 +1,27 @@
-my_counters.forEach(initCounter);
-        
-function initCounter(my_counter){
-    sanitizeParameters(my_counter);
+function sanitize(my_counter) {
+    my_counter.field = (!my_counter.field) ? '' : my_counter.field.trim();
+    my_counter.repeatable = (!my_counter.repeatable) ? '' : my_counter.repeatable.trim();
+    my_counter.editable = (!my_counter.editable) ? '' : my_counter.editable.trim();
+    my_counter.max = (!my_counter.max) ? '' : parseInt(my_counter.max);
+    my_counter.min = (!my_counter.min) ? '' : parseInt(my_counter.min);
+    my_counter.count = (!my_counter.count) ? '' : my_counter.count.trim().toLowerCase();
+    my_counter.type = (!my_counter.type) ? '' : my_counter.type.trim().toLowerCase();
+    my_counter.show = (!my_counter.show) ? '' : my_counter.show.trim().toLowerCase();
+    my_counter.label = (!my_counter.label) ? '' : my_counter.label;
+    //can't count down from nothing
+    if (my_counter.max === '') { my_counter.type = 'up'; }
+            
+    return my_counter;
+}
+function initCounter(my_counter) {
+    sanitize(my_counter);
             
     // If it's in a repeatable, switch to that function instead
-    if( my_counter.repeatable ){
-        initRepeatable(my_counter);
-        return;
-    }
+    if (my_counter.repeatable) { repeatable(my_counter); return; }
             
-    //find the right field
-    var field_name = "f_" + my_counter.editable.trim();
+    var field_name = (my_counter.field) || "f_" + my_counter.editable;
+    
     var target = document.getElementById(field_name);
-                                
     //add a character counter beneath the field  
     var counter = document.createElement('p');
     var counter_id = field_name + "_counter";
@@ -20,25 +29,24 @@ function initCounter(my_counter){
     counter.style.textAlign = 'right';
     target.parentNode.appendChild(counter);
             
-    startCounter(field_name, target, my_counter);                
+    startCounter(field_name, target, my_counter);
 }
 
-function initRepeatable(my_counter) {
+function repeatable(my_counter) {
     //listener to add counter when a new row is added
     var add_row_button = document.getElementById('addRow_f_' + my_counter.repeatable);
-    if (add_row_button && add_row_button.addEventListener){
-        add_row_button.addEventListener('click', function() {
+    if (add_row_button && add_row_button.addEventListener) {
+        add_row_button.addEventListener('click', function () {
             initCounter(my_counter);
         });
-    } else if (add_row_button && add_row_button.attachEvent){
-        add_row_button.attachEvent('onclick', function() {
+    } else if (add_row_button && add_row_button.attachEvent) {
+        add_row_button.attachEvent('onclick', function () {
             initCounter(my_counter);
         });
     }
             
     //create counters, looping through repeatable items
-    for (var i = 0;; i++){                
-        //find the right field
+    for (var i = 0;; i++) {                
         field_name = "f_";
         field_name += my_counter.repeatable + "-" + i + "-";
         field_name += my_counter.editable;
@@ -47,7 +55,7 @@ function initRepeatable(my_counter) {
         //break the loop when there are no more
         if(target == null) { break; }
         //skip it if it's already there (when adding rows to a repeatable)
-        if ( document.getElementById(field_name + '_counter') != null ) { continue; }
+        if (document.getElementById(field_name + '_counter') != null) { continue; }
                 
         //add a character counter beneath the field  
         var counter = document.createElement('p');
@@ -58,21 +66,6 @@ function initRepeatable(my_counter) {
                 
         startCounter(field_name, target, my_counter);                
     }
-}
-        
-function sanitizeParameters(my_counter) {
-    my_counter.repeatable = (!my_counter.repeatable) ? '' : my_counter.repeatable.trim();
-    my_counter.editable = (!my_counter.editable) ? '' : my_counter.editable.trim();
-    my_counter.max = (!my_counter.max) ? '' : parseInt(my_counter.max);
-    my_counter.min = (!my_counter.min) ? '' : parseInt(my_counter.min);
-    my_counter.count = (!my_counter.count) ? '' : my_counter.count.trim().toLowerCase();
-    my_counter.type = (!my_counter.type) ? '' : my_counter.type.trim().toLowerCase();
-    my_counter.show = (!my_counter.show) ? '' : my_counter.show.trim().toLowerCase();
-    my_counter.label = (!my_counter.label) ? '' : my_counter.label;
-    //can't count down from nothing
-    if(my_counter.max == '') { my_counter.type = 'up'; }
-            
-    return my_counter;
 }
         
 function startCounter(field_name, target, my_counter){
@@ -98,26 +91,28 @@ function updateCounter(field_name, max, min, type, show, label) {
     //Counter styles
     if (type == 'up'){
         //count up
-        if ( (max && count > max) || count < min ){
+        if ((max && count > max) || count < min){
             counter.children[0].style.color = 'red';
         }else{
             counter.children[0].style.color = 'green';
         }
     }else{ //count down
-        if ( count < min ){
+        if (count < min) {
             counter.children[0].style.color = 'red';
         }else{
             counter.children[0].style.color = 'green';
         }       
     }
     //Show min and/or max
-    if ( min && (show.indexOf('min') >= 0 || show.indexOf('both') >= 0) ){
+    if (min && (show.indexOf('min') >= 0 || show.indexOf('both') >= 0)) {
         //show min
         counter.innerHTML = 'Min: '+  min + '&nbsp;&nbsp;&nbsp;' + counter.innerHTML;
             }
-    if ( max && (show.indexOf('max') >= 0 || show.indexOf('both') >= 0) ){
+    if (max && (show.indexOf('max') >= 0 || show.indexOf('both') >= 0)) {
         //show max
         counter.innerHTML = counter.innerHTML + '&nbsp;&nbsp;&nbsp;Max: ' + max;
     }
     counter.innerHTML = label + '&nbsp;' + counter.innerHTML;
 }
+
+my_counters.forEach(initCounter);
