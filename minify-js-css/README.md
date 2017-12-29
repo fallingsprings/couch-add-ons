885027bc84e6e8b9d921a0083f3d8f10e3ca729b
Tim Brelig <tim@fallingspringsflowers.com>
2017-12-29 12:41:32 -0500

    Add attributes and allow Couch tags and logic

--- minify-js-css/README.md
+++ minify-js-css/README.md
@@ -23,6 +23,15 @@ The minify tag will combine and minimize CSS or JS files. It concatenates and mi
 ### output_file
 The single combined file that will be served to the page. Should be _relative to the site's root_. You can use any parameter name you want for the output file. Both of the above tags work the same.
 
+### attributes
+Any additional parameters after the output file name will pass through to the rendered tag.
+
+	<cms:minify 'css' into='css/style.min.css' media="only screen and (min-width: 800px)" >
+	
+If you wish to add an attribute to _inline_ js or css, you must explicitly declare the empty output filename in it's proper order.
+
+	<cms:minify 'js' as='' defer='defer'>
+
 ### Usage
 The `<cms:minify>` tag combines and minimizes all files in the list in the order given, storing the result in an external file. It renders a `<script>` or `<link>` tag that includes a timestamp for cache control.
 
@@ -30,7 +39,15 @@ The minimized file only gets updated when the last modification date of included
 
 If you don't specify an output file, the tag renders the output inline with a `<style>` or `<script>` tag.
 
+    <cms:minify 'css'>
+        css/bootstrap.css
+        css/bootstrap-theme.css
+        css/custom.css
+    </cms:minify>
+
 Remember that relative urls in your CSS or JS files (like fonts or background images) will now be relative to the new, minimized file. Or to the web page if rendered inline.
+
+_Note:_ In order to write the new ouput file, your server must have allow_url_fopen enabled.  If you get an empty output file - and you haven't made any mistakes with your file list - add `allow_url_fopen=1` to your php.ini file, or contact your host for help.
  
 ## Installation:
 To use the tag, unzip the attached folder into `/couch/addons/` and enable it in your `kfunctions.php` file:
--- minify-js-css/minify.php
+++ minify-js-css/minify.php
@@ -21,15 +21,24 @@ class MinifyJsCss{
         if($filetype != 'css' && $filetype != 'js'){die("ERROR: Tag \"".$node->name."\" - Must specify either 'css' or 'js'.");}
         $output_file = ($params[1]['rhs']) ? K_SITE_DIR . trim($params[1]['rhs']) : '';
         $output_link = ($output_file) ? K_SITE_URL . trim($params[1]['rhs']) : '';
-        
+        $i=0;
+        foreach($params as $attribs){
+            if($i > 1){
+                $tag_attributes .= ' ' . $attribs['lhs'] . '="' . $attribs['rhs'].'"';
+            }
+            $i++;
+        }
         //Add listed files to an array and sanitize
-        if(trim($node->children[0]->text)){
-            // get file list
-            foreach( $node->children as $child ){ $file_list .= $child->get_HTML(); }
+        foreach( $node->children as $child ){
+            $file_list .= $child->get_HTML();
+        }
+        if($file_list){
             //Split on whitespace and commas
             $files = preg_split('/[\s,+]/', $file_list, -1, PREG_SPLIT_NO_EMPTY);
+            //strip away k_site_link if present
+            $files = str_replace(K_SITE_URL, '', $files);
             //trim leading forward slash
-            foreach( $files as &$file ){ $file = ltrim($file, '/'); }
+            foreach( $files as &$file ){$file = ltrim($file, '/');}
         }else{
             die("ERROR: Tag \"".$node->name."\" - No files were listed.");
         }
@@ -43,10 +52,10 @@ class MinifyJsCss{
             }
             //No new modifications. Render 'link' or 'script' tag. Done.
             if (!$modified && $filetype == 'css'){
-                return '<link rel="stylesheet" href="'.$output_link.'?'.filemtime($output_file).'" />';
+                return '<link rel="stylesheet" href="'.$output_link.'?'.filemtime($output_file).'"'.$tag_attributes.' />';
                 }
             if (!$modified && $filetype == 'js'){
-                return '<script type="text/javascript" src="'.$output_link.'?'.filemtime($output_file).'"></script>';
+                return '<script type="text/javascript" src="'.$output_link.'?'.filemtime($output_file).'"'.$tag_attributes.'></script>';
             }
         }
         
@@ -63,17 +72,17 @@ class MinifyJsCss{
         
         //No output file. Embed output on page. Done.
         if(!$output_file){ 
-            if ($filetype == 'css'){ return '<style>' . $output . '</style>'; }
-            if ($filetype == 'js'){ return '<script type="text/javascript">' . $output . '</script>'; }
+            if ($filetype == 'css'){ return '<style' . $tag_attributes . '>' . $output . '</style>'; }
+            if ($filetype == 'js'){ return '<script type="text/javascript"' . $tag_attributes . '>' . $output . '</script>'; }
         }
         
         //Create new output file. Render tag. Done.
         file_put_contents($output_file, $output);
         if ($filetype == 'css'){
-            return '<link rel="stylesheet" href="'.$output_link.'?'.filemtime($output_file).'" />';
+            return '<link rel="stylesheet" href="'.$output_link.'?'.filemtime($output_file).'"'.$tag_attributes.' />';
         }
         if ($filetype == 'js'){
-            return '<script type="text/javascript" src="'.$output_link.'?'.filemtime($output_file).'"></script>';
+            return '<script type="text/javascript" src="'.$output_link.'?'.filemtime($output_file).'"'.$tag_attributes.'></script>';
         }
     }
 }
