532dd3e1e5aa2bd8bb3a4693b201211981aa1d70
Tim Brelig <tim@fallingspringsflowers.com>
2020-06-12 00:14:15 -1000

    cleaning and refactoring

--- character-counter/js/character-counter.js
+++ character-counter/js/character-counter.js
@@ -24,81 +24,87 @@ function initCounter(my_counter) {
                 initCounter(my_counter);
             });
         
-        //target repeatable fields
+        //set up counter for repeatable fields
         let repeatable = document.getElementById('f_' + my_counter.repeatable); 
         let my_selector = '.k_element_' + my_counter.editable + ' input';
-        var targets = [].slice.call(repeatable.querySelectorAll(my_selector));
+        var targets = repeatable.querySelectorAll(my_selector);
+        //instantiate counter
+        for (let target of targets){
+            my_counter.target = target;
+            instantiateCounter(my_counter);
+        }
     }else{
-        //target an editable field or front-end field    
+        //set up counter for editable field or front-end field    
         let field_name = (my_counter.field) || "f_" + my_counter.editable;
-        var targets = [document.getElementById(field_name)];
+        my_counter.target = document.getElementById(field_name);
+        instantiateCounter(my_counter);
     }
-   
-    //create character counter(s)
-    targets.forEach(target => {
-        let counter_id = target.id + "_counter";
+}
+
+function instantiateCounter(my_counter) {
+        my_counter.id = my_counter.target.id + "_counter";
             
         //remove a counter if it already exists (for dynamic repeatable regions)
-        if(document.getElementById(counter_id)){
-            document.getElementById(counter_id).remove();
+        if(document.getElementById(my_counter.id)){
+            document.getElementById(my_counter.id).remove();
         }
         //instantiate counter
         let counter = document.createElement('p');
-        counter.setAttribute('id', counter_id);
+        counter.setAttribute('id', my_counter.id);
         counter.style.textAlign = 'right';
-        target.parentNode.appendChild(counter);
+        my_counter.target.parentNode.appendChild(counter);
         
-        startCounter(target, counter_id, my_counter);
-    });
+        startCounter(my_counter);
 }
-        
-function startCounter(target, counter_id, my_counter){
-    //set up initial character count
-    updateCounter(target, counter_id, my_counter.max, my_counter.min, my_counter.type, my_counter.show, my_counter.label);
 
+function startCounter(my_counter){
     //Add keyboard listener to field
-        target.addEventListener('keyup', function () {
-            updateCounter(target, counter_id, my_counter.max, my_counter.min, my_counter.type, my_counter.show, my_counter.label);
-        });
-        target.addEventListener('keydown', function () {
-            updateCounter(target, counter_id, my_counter.max, my_counter.min, my_counter.type, my_counter.show, my_counter.label);
-        });
+    my_counter.target.addEventListener('keyup', function () {
+        updateCounter(my_counter);
+    });
+    my_counter.target.addEventListener('keydown', function () {
+        updateCounter(my_counter);
+    });
+    
+    //set up initial character count
+    updateCounter(my_counter);
+
 }
         
-function updateCounter(target, counter_id, max, min, type, show, label) {
-    let counter = document.getElementById(counter_id);
-    if (type == 'up'){
-        var count = target.value.length; 
+function updateCounter(my_counter) {
+    let counter = document.getElementById(my_counter.id);
+    if (my_counter.type == 'up'){
+        var count = my_counter.target.value.length; 
     }else{
-        var count = max - target.value.length;
+        var count = my_counter.max - my_counter.target.value.length;
     }
     counter.innerHTML = '<span>' + count + '</span>';
 
     //Counter styles
-    if (type == 'up'){
+    if (my_counter.type == 'up'){
         //count up
-        if ((max && count > max) || count < min){
+        if ((my_counter.max && count > my_counter.max) || count < my_counter.min){
             counter.children[0].style.color = 'red';
         }else{
             counter.children[0].style.color = 'green';
         }
     }else{ //count down
-        if (count < min) {
+        if (count < my_counter.min) {
             counter.children[0].style.color = 'red';
         }else{
             counter.children[0].style.color = 'green';
         }       
     }
     //Show min and/or max
-    if (min && (show.indexOf('min') >= 0 || show.indexOf('both') >= 0)) {
+    if (my_counter.min && (my_counter.show.indexOf('min') >= 0 || my_counter.show.indexOf('both') >= 0)) {
         //show min
-        counter.innerHTML = 'Min: '+  min + '&nbsp;&nbsp;&nbsp;' + counter.innerHTML;
+        counter.innerHTML = 'Min: '+  my_counter.min + '&nbsp;&nbsp;&nbsp;' + counter.innerHTML;
             }
-    if (max && (show.indexOf('max') >= 0 || show.indexOf('both') >= 0)) {
+    if (my_counter.max && (my_counter.show.indexOf('max') >= 0 || my_counter.show.indexOf('both') >= 0)) {
         //show max
-        counter.innerHTML = counter.innerHTML + '&nbsp;&nbsp;&nbsp;Max: ' + max;
+        counter.innerHTML = counter.innerHTML + '&nbsp;&nbsp;&nbsp;Max: ' + my_counter.max;
     }
-    counter.innerHTML = label + '&nbsp;' + counter.innerHTML;
+    counter.innerHTML = my_counter.label + '&nbsp;' + counter.innerHTML;
 }
 
 my_counters.forEach(initCounter);
